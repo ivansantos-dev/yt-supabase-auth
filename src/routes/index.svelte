913fb2c0d2e839b661235f7923355e06197a93c4
Ivan Santos <me@ivan-santos.dev>
2022-06-06 10:43:21 -0600

    Add login form

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	let email = '';
+	let password = '';
+
+	const login = () => {
+		console.log(email, password);
+	};
+	const logout = () => {
+		console.log('logout');
+	};
+</script>
+
+<svelte:head>
+	<title>SvelteKit - Supabase Auth</title>
+</svelte:head>
+
+<form on:submit|preventDefault={login}>
+	<input type="email" name="email" id="email" placeholder="email" bind:value={email} />
+	<input
+		type="password"
+		name="password"
+		id="password"
+		placeholder="password"
+		bind:value={password}
+	/>
+	<button type="submit">Login</button>
+</form>
+
+<button on:click={logout}>Logout</button>
