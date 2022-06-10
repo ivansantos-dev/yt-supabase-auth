37aab40d34808a06ba497cbf145d12f690f7f70e
Ivan Santos <me@ivan-santos.dev>
2022-06-09 22:00:24 -0600

    Add supabase auth

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,21 +1,43 @@
 <script lang="ts">
+	import { createClient } from '@supabase/supabase-js';
+	import { onMount } from 'svelte';
+
+	let anonKey =
+		'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im50cnlqa3RkbHpweXZvZXNibHR4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTQ4Mjg3NDgsImV4cCI6MTk3MDQwNDc0OH0.ALSMFKaXceVhLP2B3xyAOWHyQuOiwyePQM4-Xvi7CSc';
+	let url = 'https://ntryjktdlzpyvoesbltx.supabase.co';
+
+	let user: any;
+	const supabase = createClient(url, anonKey);
+
+	console.log(supabase);
 	let email = '';
 
-	const signInWithMagicLink = () => {
-		console.log(email);
+	const signInWithMagicLink = async () => {
+		const { user, error } = await supabase.auth.signIn({ email });
+		console.log(user, error);
 	};
+
 	const signOut = () => {
-		console.log('logout');
+		supabase.auth.signOut();
 	};
+
+	onMount(() => {
+		supabase.auth.onAuthStateChange((event, session) => {
+			user = session?.user;
+		});
+	});
 </script>
 
 <svelte:head>
 	<title>SvelteKit - Supabase Auth</title>
 </svelte:head>
 
-<form on:submit|preventDefault={signInWithMagicLink}>
-	<input type="email" name="email" id="email" placeholder="email" bind:value={email} />
-	<button type="submit">Sign In with Magic Link</button>
-</form>
-
-<button on:click={signOut}>Sign Out</button>
+{#if user}
+	<p>Signed in as {user.email}</p>
+	<button on:click={signOut}>Sign Out</button>
+{:else}
+	<form on:submit|preventDefault={signInWithMagicLink}>
+		<input type="email" name="email" id="email" placeholder="email" bind:value={email} />
+		<button type="submit">Sign In with Magic Link</button>
+	</form>
+{/if}
